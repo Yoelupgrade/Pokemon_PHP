5b5a104f2166a5f4a36b62ea4e0aa2280e8a600d
Alba <alba.m.navarro@gmail.com>
2022-02-21 12:01:32 +0100

    pokemon Alba

--- src/Controller/PokemonController.php
+++ src/Controller/PokemonController.php
@@ -2,15 +2,18 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-class PokemonController {
+
+class PokemonController extends AbstractController {
 
     /**
-     * @Route("/Pokemon")
+     * @Route("/pokemon")
      */
     public function showPokemon () {	
-        return new Response("Soy un Pokemon");
+        $pokemon=["name"=>"Charmander","id"=>4,"image"=> "https://assets.pokemon.com/assets/cms2/img/pokedex/full/004.png", "description"=> "Este Pokémon nace con una llama en la punta de la cola. Si se le apagara, fallecería." ];
+       return $this->render('pokemons/showpokemon.html.twig', ["pokemon"=>$pokemon]);
     }
 
 }
