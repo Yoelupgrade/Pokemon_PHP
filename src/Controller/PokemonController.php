8b2ac31a0bca39f85abce96cdaef3e071d2a2b73
Sergio González Gómez <sergiogonzalezgomez15@gmail.com>
2022-02-21 17:57:17 +0100

    featCosasDeSerg

--- src/Controller/PokemonController.php
+++ src/Controller/PokemonController.php
@@ -6,32 +6,53 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+use App\Entity\Pokemon;
+use Doctrine\ORM\EntityManagerInterface;
+
+
 class PokemonController extends AbstractController {
 
     /**
      * @Route("/pokemon/{id}", name="showPokemon")
      */
-    public function showPokemon ($id) {
-        $pokemons=[
-            ["name"=>"Charmander","id"=>4,"image"=> "https://assets.pokemon.com/assets/cms2/img/pokedex/full/004.png", "description"=> "Este Pokémon nace con una llama en la punta de la cola. Si se le apagara, fallecería." ],
-            ["name"=>"Mr. Mime","id"=>122,"image"=> "https://assets.pokemon.com/assets/cms2/img/pokedex/full/122.png", "description"=> "Muchos estudiosos sostienen que el desarrollo de sus enormes manos se debe a su afán por practicar la pantomima." ],
-            ["name"=>"Goldeen","id"=>118,"image"=> "https://assets.pokemon.com/assets/cms2/img/pokedex/full/118.png", "description"=> "Sus aletas pectorales, caudal y dorsal ondean gráciles en el agua. Por eso se le llama el Bailarín Acuático" ],
-        ];
+    public function showPokemon ($id, EntityManagerInterface $doctrine) {
+        
+        $repository = $doctrine->getRepository(Pokemon::class);
+        $pokemon = $repository->find($id);
 
-        return $this->render('pokemons/showpokemon.html.twig', ["pokemon"=>$pokemons[$id]]);
+        return $this->render('pokemons/showpokemon.html.twig', ["pokemon"=>$pokemon]);
     }
 
     /**
      * @Route("/pokemons", name="listPokemons")
      */
-    public function listPokemons () {	
-        $pokemons=[
-            ["name"=>"Charmander","id"=>4,"image"=> "https://assets.pokemon.com/assets/cms2/img/pokedex/full/004.png", "description"=> "Este Pokémon nace con una llama en la punta de la cola. Si se le apagara, fallecería." ],
-            ["name"=>"Mr. Mime","id"=>122,"image"=> "https://assets.pokemon.com/assets/cms2/img/pokedex/full/122.png", "description"=> "Muchos estudiosos sostienen que el desarrollo de sus enormes manos se debe a su afán por practicar la pantomima." ],
-            ["name"=>"Goldeen","id"=>118,"image"=> "https://assets.pokemon.com/assets/cms2/img/pokedex/full/118.png", "description"=> "Sus aletas pectorales, caudal y dorsal ondean gráciles en el agua. Por eso se le llama el Bailarín Acuático" ],
-        ];
+    public function listPokemons(EntityManagerInterface $doctrine) {
+        $repository = $doctrine->getRepository(Pokemon::class);	
+        $pokemons=$repository->findAll();
        return $this->render('pokemons/listPokemon.html.twig', ["pokemons"=>$pokemons]);
     }
 
+    /**
+     * @Route("/createPokemon")
+     */
+    public function insertPokemon(EntityManagerInterface $doctrine) {
+        $pokemon = new Pokemon();
+        $pokemon->setName('Charmeleon');
+        $pokemon->setDescription('Es como el otro pero más grande vale');
+        $pokemon->setImage('https://assets.pokemon.com/assets/cms2/img/pokedex/full/005.png');
+        
+        $pokemon2 = new Pokemon();
+        $pokemon2->setName('Charizard');
+        $pokemon2->setDescription('Es como el otro pero más grande todavía vale');
+        $pokemon2->setImage('https://assets.pokemon.com/assets/cms2/img/pokedex/full/006.png');
+
+
+
+        $doctrine->persist($pokemon);
+        $doctrine->persist($pokemon2);
+        $doctrine->flush();
+
+        return $this->render("base.html.twig");
+    }
 
 }
