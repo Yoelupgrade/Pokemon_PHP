75c0d7e4143576faf7ee31a12af0759a2bf6fb47
AlejandroRDev <iron-man@avengers.com>
2022-02-21 12:21:15 +0100

    listPokemons

--- src/Controller/PokemonController.php
+++ src/Controller/PokemonController.php
@@ -16,4 +16,17 @@ class PokemonController extends AbstractController {
        return $this->render('pokemons/showpokemon.html.twig', ["pokemon"=>$pokemon]);
     }
 
+      /**
+     * @Route("/pokemons")
+     */
+    public function listPokemons () {	
+        $pokemons=[
+            ["name"=>"Charmander","id"=>4,"image"=> "https://assets.pokemon.com/assets/cms2/img/pokedex/full/004.png", "description"=> "Este Pokémon nace con una llama en la punta de la cola. Si se le apagara, fallecería." ],
+            ["name"=>"Mr. Mime","id"=>122,"image"=> "https://assets.pokemon.com/assets/cms2/img/pokedex/full/122.png", "description"=> "Muchos estudiosos sostienen que el desarrollo de sus enormes manos se debe a su afán por practicar la pantomima." ],
+            ["name"=>"Goldeen","id"=>118,"image"=> "https://assets.pokemon.com/assets/cms2/img/pokedex/full/118.png", "description"=> "Sus aletas pectorales, caudal y dorsal ondean gráciles en el agua. Por eso se le llama el Bailarín Acuático" ],
+        ];
+       return $this->render('pokemons/listPokemon.html.twig', ["pokemons"=>$pokemons]);
+    }
+
+
 }
